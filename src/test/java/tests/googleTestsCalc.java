d0ba5cc49715433b4481d61d56a0e0562bdad294
Julia Kondrateva <juk88@mail.ru>
2021-05-28 00:06:04 +0300

    Добавлены test2 и test3

--- src/test/java/tests/googleTestsCalc.java
+++ src/test/java/tests/googleTestsCalc.java
@@ -3,13 +3,12 @@ package tests;
 import Pages.Buttons;
 import Pages.CalcPage;
 import Pages.SearchPage;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTestsCalc {
@@ -28,11 +27,15 @@ public class googleTestsCalc {
         calcPage = new CalcPage(driver);
     }
 
-
-    @Test
-    public void test1() {
+    @BeforeEach
+    public void setup() {
         driver.get("http://google.com");
         searchPage.search("калькулятор");
+    }
+
+    @Test
+    @DisplayName("Проверка операций с целыми числами")
+    public void test1() {
         calcPage.pressButton(Buttons.OPENED_PARENTHESIS);
         calcPage.pressButton(Buttons.ONE);
         calcPage.pressButton(Buttons.PLUS);
@@ -46,8 +49,37 @@ public class googleTestsCalc {
         calcPage.pressButton(Buttons.DIV);
         calcPage.pressButton(Buttons.FIVE);
         calcPage.pressButton(Buttons.EQUAL);
-        assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", calcPage.getMemoryString());
-        assertEquals("1", calcPage.getResult());
+
+        assertAll(
+                () -> assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", calcPage.getMemoryString()),
+                () -> assertEquals("1", calcPage.getResult())
+        );
+    }
+
+    @Test
+    @DisplayName("Проверка деления на ноль")
+    public void test2() {
+        calcPage.pressButton(Buttons.SIX);
+        calcPage.pressButton(Buttons.DIV);
+        calcPage.pressButton(Buttons.ZERO);
+        calcPage.pressButton(Buttons.EQUAL);
+
+        assertAll(
+                () -> assertEquals("6 ÷ 0 =", calcPage.getMemoryString()),
+                () -> assertEquals("Infinity", calcPage.getResult())
+        );
+    }
+
+    @Test
+    @DisplayName("Проверка ошибки при отсутствии значения")
+    public void test3() {
+        calcPage.pressButton(Buttons.SIN);
+        calcPage.pressButton(Buttons.EQUAL);
+
+        assertAll(
+                () -> assertEquals("sin() =", calcPage.getMemoryString()),
+                () -> assertEquals("Error", calcPage.getResult())
+        );
     }
 
     @AfterAll
